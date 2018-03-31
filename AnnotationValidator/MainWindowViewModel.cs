9c6df470f592950edc97c14b13ad927784beb497
Patrick Klaeren <patrick@totalren.com>
2018-03-31 01:02:00 +0100

    Initial commit

---
+++ AnnotationValidator/MainWindowViewModel.cs
@@ -0,0 +1,50 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Windows;
+using System.Windows.Input;
+
+namespace AnnotationValidator
+{
+    public class MainWindowViewModel : ViewModelBase
+    {
+        private string _name;
+        private int _age;
+
+        public MainWindowViewModel()
+        {
+            ValidateCommand = new RelayCommand(Validate);
+        }
+
+        [Required(AllowEmptyStrings = false, ErrorMessage = "A name is required for the person")]
+        public string Name
+        {
+            get => _name;
+            set
+            {
+                _name = value;
+                OnPropertyChanged(nameof(Name));
+            }
+        }
+
+        [Required(AllowEmptyStrings = false, ErrorMessage = "An age is required")]
+        [Range(0, int.MaxValue, ErrorMessage = "Please enter valid age")]
+        public int Age
+        {
+            get => _age;
+            set
+            {
+                _age = value;
+                OnPropertyChanged(nameof(Age));
+            }
+        }
+
+        public ICommand ValidateCommand { get; }
+
+        private void Validate()
+        {
+            if (!IsValid())
+                return;
+
+            MessageBox.Show("Yay!");
+        }
+    }
+}
---
+++ AnnotationValidator/ViewModelBase.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using AnnotationValidator.Annotations;
+
+namespace AnnotationValidator
+{
+    public class ViewModelBase : INotifyPropertyChanged, INotifyDataErrorInfo
+    {
+        private readonly Dictionary<string, ICollection<string>>
+            _validationErrors = new Dictionary<string, ICollection<string>>();
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public event EventHandler<DataErrorsChangedEventArgs> ErrorsChanged;
+
+        public bool HasErrors => _validationErrors.Count > 0;
+
+        public IEnumerable GetErrors(string propertyName)
+        {
+            if (string.IsNullOrEmpty(propertyName)
+                || !_validationErrors.ContainsKey(propertyName))
+                return null;
+
+            return _validationErrors[propertyName];
+        }
+
+        [NotifyPropertyChangedInvocator]
+        protected void OnPropertyChanged([CallerMemberName] string propertyName = null) =>
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+
+        protected bool IsValid()
+        {
+            _validationErrors.Clear();
+
+            ICollection<ValidationResult> validationResults = new List<ValidationResult>();
+
+            var validationContext = new ValidationContext(this, null, null);
+
+            if (Validator.TryValidateObject(this, validationContext, validationResults, true))
+                return true;
+
+            foreach (var validationResult in validationResults)
+            {
+                var property = validationResult.MemberNames.ElementAt(0);
+
+                if (_validationErrors.ContainsKey(property))
+                {
+                    _validationErrors[property].Add(validationResult.ErrorMessage);
+                }
+                else
+                {
+                    _validationErrors.Add(property, new List<string> { validationResult.ErrorMessage });
+                }
+
+                OnPropertyChanged(property);
+            }
+
+            return false;
+        }
+    }
+}
